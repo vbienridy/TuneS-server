04b33e70bcd696195164662dc18c13d975ee3597
Tao Ouyang <skyezio123@gmail.com>
2019-03-04 20:16:09 -0500

    added basic route

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+
+const users = require("./routes/api/users");
+
+app.use("/api/users", users);
+
+app.get("/", (req, res) => res.send("Hello"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
