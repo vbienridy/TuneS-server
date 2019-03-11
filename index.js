24ba5293190ddec1450b0a10c114a98aa78bce35
ZhonghengYang_minemine <mikeyyyang@outlook.com>
2019-03-11 18:51:01 -0400

    cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const passport = require("passport");
 // const cookieParser = require("cookie-parser");
 const cors = require('cors');
 const app = express()
-
+app.use(cors())
 // Passport session setup.
 passport.serializeUser(function (user, done) {
     done(null, user);
@@ -19,13 +19,7 @@ passport.deserializeUser(function (obj, done) {
 // );
 
 // Passport middleware
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+
 
 app.use(passport.initialize());
 // app.use(passport.session());
