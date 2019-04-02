56620c41999d417147e4865959931e0ae4a0d500
Tao Ouyang <skyezio123@gmail.com>
2019-04-02 16:13:44 -0400

    fixed commentSchema

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-const keys = require('./config/keys');
-const express = require('express')
+const keys = require("./config/keys");
+const express = require("express");
 const passport = require("passport");
-const cookieSession = require('cookie-session');
-const cors = require('cors');
+const cookieSession = require("cookie-session");
+const cors = require("cors");
 const app = express();
-require('./data/db')();
-app.use(express.json())    // <==== parse request body as JSON
-app.use(cors({ credentials: true, origin: keys.frontend })) // cors
-console.log("pack", cors);
+require("./data/db")();
+app.use(express.json()); // <==== parse request body as JSON
+app.use(cors({ credentials: true, origin: keys.frontend })); // cors
 
 // set up session data storing functions, essensially and magically
-passport.serializeUser(function (user, done) {
-    done(null, user);
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
-passport.deserializeUser(function (obj, done) {
-    done(null, obj);
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
 });
 
 // set up session , essensially and magically
 app.use(
-    cookieSession({ name: 'session', secret: "keyboard cat", maxAge: 60 * 60 * 1000 }) //resave: true, saveUninitialized: true
+  cookieSession({
+    name: "session",
+    secret: "keyboard cat",
+    maxAge: 60 * 60 * 1000
+  }) //resave: true, saveUninitialized: true
 );
 
 // set up passport , essensially and magically
-app.use(passport.initialize()); 
+app.use(passport.initialize());
 
 app.use(passport.session());
 
 // Passport Config
 require("./config/passport")(passport);
 
-require('./routes/userRoutes.db')(app);
-require('./routes/browseRoutes')(app);
-require('./routes/commentRoutes.db')(app);
+require("./routes/userRoutes.db")(app);
+require("./routes/browseRoutes")(app);
+require("./routes/commentRoutes.db")(app);
 require("./routes/likeRoutes.db")(app);
 
 const PORT = process.env.PORT || 5000;
-console.log('port',PORT)
 app.listen(PORT);
-
--- models/comment.schema.js
+++ models/comment.schema.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const userSchema = require("./user.schema");
 
 // commentSchema
 const commentSchema = new mongoose.Schema(
   {
-    // user: { type: mongoose.Schema.Types.ObjectId, ref: "UserModel" },
-    user: userSchema,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "UserModel" },
+    // user: userSchema,
     content: String,
     subjectType: String,
     subjectId: String,
