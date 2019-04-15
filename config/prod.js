29b3cd75afe47714db2220c01ffdced88f4e66d7
ZhonghengYang_minemine <mikeyyyang@outlook.com>
2019-04-14 23:38:36 -0400

    solve cors

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,5 @@ module.exports = {
   callBackURL: process.env.SPOTIFY_CALLBACK_URL,
   mongodb:process.env.MONGODB_URL,
   // frontend:["https://tune-s.herokuapp.com", "http://tune-s.herokuapp.com"]
-  frontend: ["https://tunes-app.herokuapp.com", "http://tunes-app.herokuapp.com"]
+  frontend: ["https://tune-s.herokuapp.com", "http://tune-s.herokuapp.com"]
 };
