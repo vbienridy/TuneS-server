d4e0beab67756df88d739db9b5fa2347aea03583
ZhonghengYang_minemine <mikeyyyang@outlook.com>
2019-04-15 00:17:40 -0400

    fixed db

--- data/db.js
+++ data/db.js
@@ -1,8 +1,8 @@
 module.exports = function() {
   const keys = require("../config/keys");
   const mongoose = require("mongoose");
-  const databaseName = "tunes";
-  const connectionString = keys.mongodb + databaseName;
+  // const databaseName = "tunes";
+  const connectionString = keys.mongodb
   mongoose.connect(connectionString, { useNewUrlParser: true });
 
   const db = mongoose.connection;
