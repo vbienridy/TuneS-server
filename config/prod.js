558d2e8060f0fb452da4de7befd54c556ede5978
Zexi Han <zexihan@me.com>
2019-04-18 13:32:25 -0400

    switched frontend url

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,10 @@ module.exports = {
   spotifyClientID: process.env.SPOTIFY_CLIENT_ID,
   spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
   callBackURL: process.env.SPOTIFY_CALLBACK_URL,
-  mongodb:process.env.MONGODB_URL,
+  mongodb: process.env.MONGODB_URL,
   // frontend:["https://tune-s.herokuapp.com", "http://tune-s.herokuapp.com"]
-  frontend: ["https://tune-s.herokuapp.com", "http://tune-s.herokuapp.com"]
+  frontend: [
+    "https://tunes-app.herokuapp.com",
+    "http://tunes-app.herokuapp.com"
+  ]
 };
